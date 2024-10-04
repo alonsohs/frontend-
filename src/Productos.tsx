5ef2f70e573b374815afa590be11578a61639765
Enanojr <efrenmoto@live.com>
2024-10-04 14:02:56 -0600

    asdasd

--- src/Productos.tsx
+++ src/Productos.tsx
@@ -1,33 +1,18 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
 import { seccion } from "./Producto";
 import { Serie } from "./Producto";
 import { Seccion_get } from "./Services/cuadro.service";
 //import { serie_get } from "./services/cuadro.service";
 import { serie_post } from "./Services/cuadro.service";
 import { Boton } from "./components/Botones/Botones";
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 export const Productos = () => {
   //const [productos, setProductos] = useState<Producto[]>([]);
   const [secciones, setSeccion] = useState<seccion[]>([]);
   const [serie, setSerie] = useState<Serie[]>([]);
   const [id_seccion, setId_seccion] = useState("");
-=======
-import { Producto } from "./Producto";
-import { Serie } from "./Producto";
-import { Seccion_get } from "./services/cuadro.service";
-import { serie_get } from "./services/cuadro.service";
-import { serie_post } from "./services/cuadro.service";
-import { Boton } from "./components/Botones/Botones";
-
-export const Productos = () => {
-  const [productos, setProductos] = useState<Producto[]>([]);
-  const [secciones, setSeccion] = useState<Producto[]>([]);
-  const [serie, setSerie] = useState<Serie[]>([]);
->>>>>>> eefc4dce919d884c99bad0a732faa258d148d107
   const [ID, setID] = useState("");
   const [Serie, setSe] = useState("");
   const [Codigo, setCode] = useState("");
@@ -47,14 +32,9 @@ export const Productos = () => {
     const serie = {
       id_serie: ID,
       serie: Serie,
-<<<<<<< HEAD
       codigo_serie: Codigo,
       descripcion: Descripcion,
-      id_seccion: id_seccion
-=======
-      codigo: Codigo,
-      descripcion: Descripcion,
->>>>>>> eefc4dce919d884c99bad0a732faa258d148d107
+      id_seccion: id_seccion,
     };
 
     try {
@@ -64,18 +44,21 @@ export const Productos = () => {
       console.error("Error:", error);
     }
   };
-<<<<<<< HEAD
 
   const columns: GridColDef[] = [
-    { field: 'codigo', headerName: 'codigo', width: 150 },
-    { field: 'descripcion', headerName: 'descripcion', width: 150 },
+    { field: "codigo", headerName: "codigo", width: 150 },
+    { field: "descripcion", headerName: "descripcion", width: 150 },
   ];
-  
 
   return (
     <div>
       <form action="" onSubmit={handleSubmit}>
-        <select name="seccion" id="seccion"value={id_seccion} onChange={(e) => setId_seccion(e.target.value)} >
+        <select
+          name="seccion"
+          id="seccion"
+          value={id_seccion}
+          onChange={(e) => setId_seccion(e.target.value)}
+        >
           {" "}
           Seccion
           {secciones.map((seccion) => (
@@ -104,67 +87,19 @@ export const Productos = () => {
         />
         <Boton>Enviar</Boton>
       </form>
-
-      <div style={{ height: 300, width: '100%' }}>
-      <DataGrid rows={secciones} columns={columns} getRowId={(x)=> x.id_seccion}/>
-
-    </div>
-
-    
-=======
-  return (
-    <div>
-      <form action="" onSubmit={handleSubmit}>
-        <select name="seccion" id="seccion">
-          {" "}
-          Seccion
-          {secciones.map((seccion) => (
-            <option value={seccion.id_seccion}> {seccion.id_seccion}</option>
-          ))}
-        </select>
-        <label>ID serie</label>
-        <input type="text" value={ID} onChange={(e) => setID(e.target.value)} />
-        <label>Serie</label>
-        <input
-          type="text"
-          value={Serie}
-          onChange={(e) => setSe(e.target.value)}
+      <div style={{ height: 300, width: "100%" }}>
+        <DataGrid
+          rows={secciones}
+          columns={columns}
+          getRowId={(x) => x.id_seccion}
         />
-        <label>Codigo Serie</label>
-        <input
-          type="text"
-          value={Codigo}
-          onChange={(e) => setCode(e.target.value)}
-        />
-        <label>Descripcion</label>
-        <input
-          type="text"
-          value={Descripcion}
-          onChange={(e) => setDescripcion(e.target.value)}
-        />
-        <Boton>Enviar</Boton>
-      </form>
-
-      {productos.map((producto) => (
-        <div key={producto.id_seccion} {...producto}>
-          <p>Seccion: {producto.id_seccion}</p>
-          <p>Codigo: {producto.codigo}</p>
-          <p>Descripcion: {producto.descripcion}</p>
-        </div>
-      ))}
-
->>>>>>> eefc4dce919d884c99bad0a732faa258d148d107
+      </div>
       {/*<select name="serie" id="serie">
         {serie.map((serie) => (
           <option value={serie.serie}>{serie.serie}</option>
         ))}
       </select>*/}
-<<<<<<< HEAD
-    </div>
-  );
-};
-=======
+        
     </div>
   );
 };
->>>>>>> eefc4dce919d884c99bad0a732faa258d148d107
